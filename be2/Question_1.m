d9d9156f34e31f7f9cfa2480d827e0108d47d70a
JulesV99 <giulianovinci99@gmail.com>
2022-12-09 08:23:18 +0100

    Here is the first push after the BE class

--- be2/Question_1.m
+++ be2/Question_1.m
@@ -11,39 +11,66 @@ rand('seed',1)
 randn('seed',1)
 nt = 1500;
 %To avoid numerical dispersion and instability, dx < cmin/(5*fpeak) and dt < 0.6*dx/cmax.
-%
-dx = 5;
-dt = 0.001;
-nz = 301;
-c_true = 2000*ones(nz,1);
-c_true(51:80) = 1600;
-c_true(121:150) = 2500;
-c_true(200:210) = 1800;
-cmin=min(c_true);
-cmax=max(c_true);
-zs = 5; % The source position
-f1 = 5; % The peak frequency
-print=0;
-source=SourceTerm(nt,f1,dt,print);
-p0 = zeros(nz,1);
-p1 = p0;
-p0(zs) = source(1);
-p1(zs) = source(2);
-[p_true,p_all] = ForwardProblem(nz,nt,source,c_true,zs,dx,dt,p0,p1,print);
-p_obs=p_true; % possiblly with noise
-%
-n = 100;
-t = linspace(-2,2,n);  % t \in [-2, 2]
-%
-% define vect_f such as vect_f(i) = obj_fun(c+ t(i) dc)
-%
-for i =1:n
+figure(3)
+hold on
+values = linspace(0,1,5);
+lbl = cell(size(values));
+unit = 'm/s';
+quant = '\sigma_c';
+quant_lab = 'c_noise';
+
+for v = 1:length(values)
+    value = values(v);
+    
+    dx = 5;
+    dt = 0.001;
+    nz = 301;
+    c_true = 2000*ones(nz,1);
+    c_true(51:80) = 1600;
+    c_true(121:150) = 2500;
+    c_true(200:210) = 1800;
+    c_true = c_true + 200*value*rand(size(c_true));
+    
+    cmin=min(c_true);
+    cmax=max(c_true);
+    zs = 5; % The source position
+
+    f1 = 50; % The peak frequency 
+    print=0;
+    source=SourceTerm(nt,f1,dt,print);
+    p0 = zeros(nz,1);
+    p1 = p0;
+    p0(zs) = source(1);
+    p1(zs) = source(2);
+    [p_true,p_all] = ForwardProblem(nz,nt,source,c_true,zs,dx,dt,p0,p1,print);
+    p_obs = p_true ; % possiblly with noise
     %
-    % To Do
+    n = 100;
+    t = linspace(-2,2,n);  % t \in [-2, 2]
+    c0 = c_true;
+    c1 = c0 + 0.1*c0;
     %
-    vect_f(i)=
+    % define vect_f such as vect_f(i) = obj_fun(c+ t(i) dc)
+    %
+
+    for i =1:n
+        
+        vect_f(i) = CostFunc_FWI(c0 + t(i)*(c1-c0)); %#ok<*SAGROW>
+    end
+
+plot(t, vect_f,'LineWidth',2);
+lbl{v} = ['$',quant, ' = ',num2str(value*200),' ',unit,'$'];
 end
-figure(3);
-plot(t, vect_f,'k','LineWidth',2);
-xlabel({'t'});
-ylabel({'Obj. Func.'});
+
+xl = xlabel({'t'});
+set(xl, 'Interpreter', 'latex', 'fontsize', 16 , 'LineWidth', 1.5);
+yl = ylabel({'Obj. Func.'});
+set(yl, 'Interpreter', 'latex', 'fontsize', 16 , 'LineWidth', 1.5);
+leg = legend(lbl);
+set(leg, 'Interpreter', 'latex', 'fontsize', 14)
+ax = gca;
+ax.FontSize = 16;
+grid minor
+titles = ['Quest3 for var ', quant_lab];
+clear print
+print(3,titles,'-deps');
--- be2/Questions_2_3_4_5.m
+++ be2/Questions_2_3_4_5.m
@@ -30,61 +30,88 @@ p0 = zeros(nz,1);
 p1 = p0;
 p0(zs) = source(1);
 p1(zs) = source(2);
+values = [1950 2000 2050 2100];
 [p_true,p_all]=ForwardProblem(nz,nt,source,c_true,zs,dx,dt,p0,p1,print);
-sig = 0;
-p_obs=p_true+sig*randn(1)*p_true; % possiblly with noise
+
+
+
+lbl = cell(1,length(values)+1);
 t = (0:nt-1)'*dt;
 z = (0:nz-1)'*dx;
+figure(4)
+hold on
+plot(z,c_true,'b', 'LineWidth', 1.5)
+lbl{1} = 'True Velocity';
+quant_lab = 'c0';
 %
 % For this starting point c_0
 %
-c = 2000*ones(nz,1); 
+for v = 1:length(values)
+    value = values(v);
+    
+    sig = 0.1;
+    p_obs=p_true+sig*randn(1)*p_true; % possiblly with noise
 
-fprintf('\n**********************\nRunning Algo Descent : peak frequency f= %d\n',f1);
-fprintf('it\t obj\t\t norm(d)\t step\n');
-n_iter = 50;
-tolg=1e-10; % Tol. of the gradient.
+    c = value*ones(nz,1); 
+
+    fprintf('\n**********************\nRunning Algo Descent : peak frequency f= %d\n',f1);
+    fprintf('it\t obj\t\t norm(d)\t step\n');
+    n_iter = 200;
+    tolg=1e-10; % Tol. of the gradient.
 for i=1:n_iter 
     %
     %    evaluate the objective function and its gradient
     %    (To do)
-    [objfun,g]=
+    [objfun,g]= CostFunc_FWI(c);
         %
         %  Update the step size alpha and the velocity profile c
         %  (To do ) 
-        alpha=
+        alpha = 100/norm(g);
         %
         % Compute the search direction 
         %
-        d = 
+        d = -g;
         %
         % Update the velocity profile
         %
-        c= 
+        c = c + alpha*d;
         %
         %
         fprintf('%d\t %.3e\t %.3e\t %.3e \n',i,objfun,norm(g), alpha);
         %
         % DISPLAY FIGURE
         %
-    figure(3)
-    plot(z,c_true,'b',z,c,'r');legend('True Velocity','Inverted Velocity');
-    xlabel('Depth (m)');ylabel('Velocity (m/s)');
-    title('Low frequency inversion');
-    axis([0 1500 1000 3000]);
-    drawnow;
+%     figure(3)
+%     plot(z,c_true,'b',z,c,'r');legend('True Velocity','Inverted Velocity');
+%     xlabel('Depth (m)');ylabel('Velocity (m/s)');
+%     title('Low frequency inversion');
+%     axis([0 1500 1000 3000]);
+%     drawnow;
     %
     % Check stopping criteria 
     %
-    if( ) % Define your stopping criteria
+    if(norm(g) < tolg) % Define your stopping criteria
         break;
     end
 end
-figure(4)
-plot(z,c_true,'b',z,c,'k');legend('True Velocity','Inverted Velocity');
-xlabel('Depth (m)');ylabel('Velocity (m/s)');
-title('Inversion using  Descent Algo');
+plot(z,c,'LineWidth', 1.1);
+lbl{v+1} = ['$c_0 $ = ', num2str(value)];
+
 axis([0 1500 1000 3000]);
+end
+% ttl = title('Inversed Velocity using  Descent Algo');
+% set(ttl, 'Interpreter', 'latex', 'fontsize', 20);
+grid minor
+xl = xlabel('Depth (m)');
+yl = ylabel('Velocity (m/s)');
+set(yl, 'Interpreter', 'latex', 'fontsize', 16 , 'LineWidth', 1.5);
+set(xl, 'Interpreter', 'latex', 'fontsize', 16 , 'LineWidth', 1.5);
+leg = legend(lbl);
+set(leg, 'Interpreter', 'latex', 'fontsize', 14 , 'LineWidth', 1.5, 'Location', 'southeast');
+clear print
+titles = ['Quest4 for var', quant_lab];
+print(4, titles, '-deps')
+
 %figure(5)
 %plot(residual);title('Residual over iterations');
 %xlabel('Iteration');ylabel('Residual');
